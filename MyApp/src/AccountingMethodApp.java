0a1b183557116cde82642ff89f7ba169f859acc7
Woojin Lee <dhkep03@gmail.com>
2022-08-06 15:48:54 +0900

    14-9

---
+++ MyApp/src/AccountingMethodApp.java
@@ -0,0 +1,36 @@
+public class AccountingMethodApp {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        double valueOfSupply = Double.parseDouble(args[0]);
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double vat = getVAT(valueOfSupply, vatRate);
+        double total = valueOfSupply + vat;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - expense;
+
+        double[] dividendRates = new double[3];
+        dividendRates[0] = 0.5;
+        dividendRates[1] = 0.3;
+        dividendRates[2] = 0.2;
+
+        double dividend1 = income * dividendRates[0];
+        double dividend2 = income * dividendRates[1];
+        double dividend3 = income * dividendRates[2];
+
+        System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + vat);
+        System.out.println("Total : " + total);
+        System.out.println("Expense : " + expense);
+        System.out.println("Income : " + income);
+        System.out.println("Dividend 1 : " + dividend1);
+        System.out.println("Dividend 2 : " + dividend2);
+        System.out.println("Dividend 3 : " + dividend3);
+
+    }
+
+    private static double getVAT(double valueOfSupply, double vatRate) {
+        return valueOfSupply * vatRate;
+    }
+}
