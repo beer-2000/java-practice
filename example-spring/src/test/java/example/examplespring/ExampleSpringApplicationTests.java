94b7b18c43c2bb061708f7a1760b69dc2acd0ed9
Woojin Lee <dhkep03@gmail.com>
2022-08-09 01:59:00 +0900

    Spring project start

---
+++ example-spring/src/test/java/example/examplespring/ExampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package example.examplespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
