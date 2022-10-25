421977d0b19fcc9a6b3a0136000040dde6f93b13
Woojin Lee <dhkep03@gmail.com>
2022-10-25 23:26:08 +0900

    jump to java - class 6

--- JumpToJavaRepeat/src/class6/Sample1.java
+++ JumpToJavaRepeat/src/class6/Sample1.java
@@ -1,2 +1,20 @@
-package class6;public class Sample1 {
+package class6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Sample1 {
+    public static void main(String[] args) throws IOException {
+//        InputStream in = System.in;
+//        InputStreamReader reader = new InputStreamReader(in);
+//        BufferedReader br = new BufferedReader(reader);
+//
+//        String a = br.readLine();
+//        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+    }
 }
--- JumpToJavaRepeat/src/class6/Sample2.java
+++ JumpToJavaRepeat/src/class6/Sample2.java
@@ -1,2 +1,35 @@
-package class6;public class Sample2 {
+package class6;
+
+import java.io.*;
+
+public class Sample2 {
+    public static void main(String[] args) throws IOException {
+        PrintWriter pw = new PrintWriter("./out.txt");
+        for(int i=1; i<11; i++) {
+            String data = i+" 번째 줄입니다.";
+            pw.println(data);
+        }
+        pw.close();
+
+        FileWriter fw2 = new FileWriter("./out.txt", true);  // 파일을 추가 모드로 연다.
+        for(int i=11; i<21; i++) {
+            String data = i+" 번째 줄입니다.\r\n";
+            fw2.write(data);
+        }
+        fw2.close();
+
+        byte[] b = new byte[100];
+        FileInputStream input = new FileInputStream("./out.txt");
+        input.read(b);
+        System.out.println(new String(b));  // byte 배열을 문자열로 변경하여 출력
+        input.close();
+
+        BufferedReader br = new BufferedReader(new FileReader("./out.txt"));
+        while(true) {
+            String line = br.readLine();
+            if (line==null) break;  // 더 이상 읽을 라인이 없을 경우 while 문을 빠져나간다.
+            System.out.println(line);
+        }
+        br.close();
+    }
 }
