79757e244724f80dee90780e99c3cb54d4ff4fc9
Woojin Lee <dhkep03@gmail.com>
2022-10-25 20:04:29 +0900

    jump to java - class 4

--- JumpToJavaRepeat/src/class4/Example.java
+++ JumpToJavaRepeat/src/class4/Example.java
@@ -1,2 +1,13 @@
-package class4;public class Example {
+package class4;
+
+public class Example {
+    public static void main(String[] args) {
+        int[] marks = {70, 60, 55};
+        int sum = 0;
+        for (int mark: marks) {
+            sum += mark;
+        }
+        float average = (float) sum / marks.length;
+        System.out.println(average);
+    }
 }
