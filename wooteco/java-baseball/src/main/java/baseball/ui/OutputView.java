c617ce51606be54849ec625ca9ee15c39fa51240
Woojin Lee <dhkep03@gmail.com>
2022-12-07 19:55:48 +0900

    feat(OutputView): 숫자 입력 멘트 출력

--- wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
+++ wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
@@ -2,9 +2,14 @@ package baseball.ui;
 
 public class OutputView {
     private String ANNOUNCEMENT_START_GAME = "숫자 야구 게임을 시작합니다.";
+    private String REQUEST_INPUT_NUMBER = "숫자를 입력해주세요 : ";
 
     public void announceStartGame() {
         System.out.println(ANNOUNCEMENT_START_GAME);
+    }
+
+    public void requestInputNumber() {
+        System.out.print(REQUEST_INPUT_NUMBER);
     }
 
     public void printResultOfTurn(String resultSentence) {
