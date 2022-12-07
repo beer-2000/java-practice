853665e297fa45e4710e32680f76dd22022a3865
Woojin Lee <dhkep03@gmail.com>
2022-12-07 20:37:51 +0900

    feat(GameResult): 볼 추가

--- wooteco/java-baseball/src/main/java/baseball/domain/GameResult.java
+++ wooteco/java-baseball/src/main/java/baseball/domain/GameResult.java
@@ -7,9 +7,23 @@ public class GameResult {
     public GameResult(int balls, int strikes) {
         this.balls = balls;
         this.strikes = strikes;
+        String BALL_CHARACTER = "볼 ";
+        String STRIKE_CHARACTER = "스트라이크 ";
     }
 
     public boolean isOut() {
         return this.strikes == 3;
+    }
+
+    public String getSentence() {
+        StringBuilder sentenceOfResult = new StringBuilder("");
+        sentenceOfResult.append(getSentenceOfBalls());
+    }
+
+    private String getSentenceOfBalls() {
+        if (balls == 0) {
+            return "";
+        }
+        return balls + "볼 ";
     }
 }
