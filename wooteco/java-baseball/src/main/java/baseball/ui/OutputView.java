4d2e0056caa3be7b09d6013fa08f8cf0a492e091
Woojin Lee <dhkep03@gmail.com>
2022-12-07 19:56:57 +0900

    feat(OutputView): 게임 종료 안내 멘트 출력

--- wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
+++ wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
@@ -3,6 +3,7 @@ package baseball.ui;
 public class OutputView {
     private String ANNOUNCEMENT_START_GAME = "숫자 야구 게임을 시작합니다.";
     private String REQUEST_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private String ANNOUNCEMENT_OUT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void announceStartGame() {
         System.out.println(ANNOUNCEMENT_START_GAME);
@@ -14,5 +15,9 @@ public class OutputView {
 
     public void printResultOfTurn(String resultSentence) {
         System.out.println(resultSentence);
+    }
+
+    public void announceOut() {
+        System.out.println(ANNOUNCEMENT_OUT);
     }
 }
