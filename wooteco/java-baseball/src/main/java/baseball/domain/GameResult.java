816cf50ab50dd2e3bdb7c3bf2be061e6ec5648f3
Woojin Lee <dhkep03@gmail.com>
2022-12-07 20:30:17 +0900

    feat(GameResult): 정답 여부 확인

--- wooteco/java-baseball/src/main/java/baseball/domain/GameResult.java
+++ wooteco/java-baseball/src/main/java/baseball/domain/GameResult.java
@@ -1,4 +1,15 @@
 package baseball.domain;
 
 public class GameResult {
+    private int balls;
+    private int strikes;
+
+    public GameResult(int balls, int strikes) {
+        this.balls = balls;
+        this.strikes = strikes;
+    }
+
+    public boolean isOut() {
+        return this.strikes == 3;
+    }
 }
