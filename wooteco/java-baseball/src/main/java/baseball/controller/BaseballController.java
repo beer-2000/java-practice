29224e88799366848ccf1ba8c58c3cdb92aa1fe0
Woojin Lee <dhkep03@gmail.com>
2022-12-07 19:51:07 +0900

    feat(BaseballController): 프로젝트 실행 구조 설계

--- wooteco/java-baseball/src/main/java/baseball/controller/BaseballController.java
+++ wooteco/java-baseball/src/main/java/baseball/controller/BaseballController.java
@@ -1,18 +1,44 @@
 package baseball.controller;
 
-public class BaseballController {
+import baseball.domain.BaseballGame;
+import baseball.domain.GameResult;
+import baseball.ui.InputView;
+import baseball.ui.OutputView;
 
-//    public void play() {
-//        do {
-//            playGame();
-//        } while (baseballGame.isEnd());
-//    }
-//
-//    public void playGame() {
-//        baseballGame.init();
-//        do {
-//            결과 = playTurn();
-//        } while (결과.정답());
-//        baseballGame.재시작_판단(재시작_여부);
-//    }
+public class BaseballController {
+    private BaseballGame baseballGame;
+    private InputView inputView;
+    private OutputView outputView;
+
+    public BaseballController() {
+        this.baseballGame = new BaseballGame();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void play() {
+        outputView.announceStartGame();
+        do {
+            playGame();
+        } while (baseballGame.isEnd());
+    }
+
+    public void playGame() {
+        baseballGame.init();
+        do {
+            GameResult gameResult = playTurn();
+            outputView.printResultOfTurn(gameResult.getSentence());
+        } while (GameResult.isOut());
+        outputView.announceOut();
+        outputView.requestInputOfRestartCommand();
+        String restartCommand = inputView.inputRestartCommand();
+        baseballGame.judgeRestart(restartCommand);
+    }
+
+    public GameResult playTurn() {
+        outputView.requestInputNumber();
+        String inputNumberRaw = inputView.inputNumber();
+        GameResult gameResult = baseballGame.judge(inputNumberRaw);
+        return gameResult;
+    }
 }
