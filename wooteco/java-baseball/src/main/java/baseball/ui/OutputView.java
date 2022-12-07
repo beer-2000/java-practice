7483212629f6dc6aa5454c9a47bc2950e00a3705
Woojin Lee <dhkep03@gmail.com>
2022-12-07 19:57:50 +0900

    feat(OutputView): 게임 재시작 여부 입력 안내 멘트 출력

--- wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
+++ wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
     private String ANNOUNCEMENT_START_GAME = "숫자 야구 게임을 시작합니다.";
     private String REQUEST_INPUT_NUMBER = "숫자를 입력해주세요 : ";
     private String ANNOUNCEMENT_OUT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private String REQUEST_INPUT_RESTART_COMMAND = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void announceStartGame() {
         System.out.println(ANNOUNCEMENT_START_GAME);
@@ -19,5 +20,9 @@ public class OutputView {
 
     public void announceOut() {
         System.out.println(ANNOUNCEMENT_OUT);
+    }
+
+    public void requestInputOfRestartCommand() {
+        System.out.println(REQUEST_INPUT_RESTART_COMMAND);
     }
 }
