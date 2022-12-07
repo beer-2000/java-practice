cc0c42436a070929d78a70de65424cfc1593adba
Woojin Lee <dhkep03@gmail.com>
2022-12-07 19:34:45 +0900

    feat: 프로젝트 구조 설계

---
+++ wooteco/java-baseball/src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,18 @@
+package baseball.controller;
+
+public class BaseballController {
+
+//    public void play() {
+//        do {
+//            playGame();
+//        } while (baseballGame.isEnd());
+//    }
+//
+//    public void playGame() {
+//        baseballGame.init();
+//        do {
+//            결과 = playTurn();
+//        } while (결과.정답());
+//        baseballGame.재시작_판단(재시작_여부);
+//    }
+}
---
+++ wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
@@ -0,0 +1,4 @@
+package baseball.ui;
+
+public class OutputView {
+}
