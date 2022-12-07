f9e9c9dd8d7fe65a82d359395d6b72bbedaf33a6
Woojin Lee <dhkep03@gmail.com>
2022-12-07 20:05:48 +0900

    feat(BaseballGame): 게임 초기화

--- wooteco/java-baseball/src/main/java/baseball/domain/BaseballGame.java
+++ wooteco/java-baseball/src/main/java/baseball/domain/BaseballGame.java
@@ -1,4 +1,12 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class BaseballGame {
+    private List<Integer> target;
+
+    public void init() {
+        TargetMaker targetMaker = new TargetMaker();
+        this.target = targetMaker.generateTargetNumbers();
+    }
 }
