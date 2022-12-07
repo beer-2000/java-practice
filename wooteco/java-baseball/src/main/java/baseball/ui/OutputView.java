cd61e3f2413df260e873d48dcffcfdc9d805f558
Woojin Lee <dhkep03@gmail.com>
2022-12-07 19:52:50 +0900

    feat(OutputView): 게임 시작 안내 멘트 출력

--- wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
+++ wooteco/java-baseball/src/main/java/baseball/ui/OutputView.java
@@ -1,4 +1,9 @@
 package baseball.ui;
 
 public class OutputView {
+    private String ANNOUNCEMENT_START_GAME = "숫자 야구 게임을 시작합니다.";
+
+    public void announceStartGame() {
+        System.out.println(ANNOUNCEMENT_START_GAME);
+    }
 }
