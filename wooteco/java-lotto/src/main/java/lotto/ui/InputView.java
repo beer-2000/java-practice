e63cef0b8ab6d7abb67df8a3b46af14aa7ccce2a
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:27:22 +0900

    feat(InputView): 당첨 번호 입력 받기

--- wooteco/java-lotto/src/main/java/lotto/ui/InputView.java
+++ wooteco/java-lotto/src/main/java/lotto/ui/InputView.java
@@ -3,8 +3,12 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    
+
     public String getMoneyByUser() {
+        return Console.readLine();
+    }
+
+    public String getTargetNumbersFromUser() {
         return Console.readLine();
     }
 }
