3443011f15f837576cc72aadbc60dfea39d75fe8
Woojin Lee <dhkep03@gmail.com>
2022-12-09 16:43:47 +0900

    feat: 프로그램 실행 로직 설정

--- wooteco/java-lotto/src/main/java/lotto/controller/LottoController.java
+++ wooteco/java-lotto/src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,26 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.WinningCalculator;
+import lotto.ui.InputView;
+import lotto.ui.OutputView;
+
 public class LottoController {
+    private InputView inputView;
+    private OutputView outputView;
+    private WinningCalculator winningCalculator;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        outputView.announceInputTargetNumbers();
+        outputView.announceInputBonusNumber();
+        String targetNumbersRaw = inputView.getTargerNumbersFromUser();
+        String bonusNumberRaw = inputView.getBonusNumberFromUser();
+        this.winningCalculator = new WinningCalculator(targetNumbersRaw, bonusNumberRaw);
+    }
+
+    public void purchaseLottos() {
+
+    }
 }
