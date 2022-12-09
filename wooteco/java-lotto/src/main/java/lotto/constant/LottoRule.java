b55d0f1b3ac2147faacf27d4c17e973683363460
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:16:21 +0900

    feat(NumberGenerator): 번호 생성

--- wooteco/java-lotto/src/main/java/lotto/constant/LottoRule.java
+++ wooteco/java-lotto/src/main/java/lotto/constant/LottoRule.java
@@ -2,4 +2,7 @@ package lotto.constant;
 
 public class LottoRule {
     public static final int PRICE = 1_000;
+    public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 45;
+    public static final int COUNT_OF_NUMBERS = 6;
 }
--- wooteco/java-lotto/src/main/java/lotto/domain/NumbersGenerator.java
+++ wooteco/java-lotto/src/main/java/lotto/domain/NumbersGenerator.java
@@ -1,4 +1,17 @@
 package lotto.domain;
 
+import static lotto.constant.LottoRule.COUNT_OF_NUMBERS;
+import static lotto.constant.LottoRule.MAXIMUM_NUMBER;
+import static lotto.constant.LottoRule.MINIMUM_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class NumbersGenerator {
+    public static List<Integer> generateNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                MINIMUM_NUMBER, MAXIMUM_NUMBER, COUNT_OF_NUMBERS
+        );
+        return numbers;
+    }
 }
