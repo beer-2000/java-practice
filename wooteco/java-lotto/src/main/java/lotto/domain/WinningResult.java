4cfba7d64a3a24abeaa4cc40e1cc63669757bfec
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:58:17 +0900

    feat(WinningResult): 당첨 결과 상수 집합 구현

--- wooteco/java-lotto/src/main/java/lotto/domain/WinningResult.java
+++ wooteco/java-lotto/src/main/java/lotto/domain/WinningResult.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
-public class WinningResult {
+public enum WinningResult {
+    NONE(0),
+    FIFTH(3),
+    FOURTH(4),
+    THIRD(5),
+    SECOND(5),
+    FIRST(6);
+
+    private int matchCount;
+
+    WinningResult(int matchCount) {
+        this.matchCount = matchCount;
+    }
 }
