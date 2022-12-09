73132844cf56d5ff03a6a9bfb71352d9579b7d72
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:20:32 +0900

    refactor(OutputView): 줄띄움 추가

--- wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
+++ wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
@@ -5,13 +5,16 @@ import java.util.List;
 public class OutputView {
     private String REQUEST_INPUT_AMOUNT = "구입금액을 입력해 주세요.";
     private String ANNOUNCEMENT_PURCHASE_COUNT = "%d개를 구매했습니다.";
+    private String LINE_SEPARATOR = System.lineSeparator();
 
     public void announceInputMoney() {
         System.out.println(REQUEST_INPUT_AMOUNT);
     }
 
     public void announcePurchaseCount(int purchaseCount) {
+        System.out.println();
         System.out.printf(ANNOUNCEMENT_PURCHASE_COUNT, purchaseCount);
+        System.out.println();
     }
 
     public void printNumbers(List<Integer> numbers) {
