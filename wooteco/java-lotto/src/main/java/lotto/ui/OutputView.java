0127c5db9c9888e5362a6f99965212b07b0cfec3
Woojin Lee <dhkep03@gmail.com>
2022-12-09 16:45:12 +0900

    feat(OutputView): 구입금액 입력 안내 멘트 출력

--- wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
+++ wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
@@ -1,4 +1,9 @@
 package lotto.ui;
 
 public class OutputView {
+    private String REQUEST_INPUT_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public void announceInputTargetNumbers() {
+        System.out.println(REQUEST_INPUT_AMOUNT);
+    }
 }
