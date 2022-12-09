a51ca8925be1bc065d1b59629d165af3e4700678
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:57:57 +0900

    feat(ResultTable): 초깃값 설정

--- wooteco/java-lotto/src/main/java/lotto/domain/ResultTable.java
+++ wooteco/java-lotto/src/main/java/lotto/domain/ResultTable.java
@@ -1,4 +1,18 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ResultTable {
+    private Map<WinningResult, Integer> table;
+
+    public ResultTable() {
+        Map<WinningResult, Integer> table = new LinkedHashMap<>();
+        Arrays.stream(WinningResult.values())
+                .forEach(winningResult -> {
+                    table.put(winningResult, 0);
+                });
+        this.table = table;
+    }
 }
