1b945eb60c3b75d47921b473a9d34465404eccf8
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:01:06 +0900

    feat: 로또 구매 로직 작성

--- wooteco/java-lotto/src/main/java/lotto/controller/LottoController.java
+++ wooteco/java-lotto/src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import lotto.domain.Cashier;
+import lotto.domain.Lotto;
+import lotto.domain.NumbersGenerator;
+import lotto.domain.ResultTable;
 import lotto.domain.WinningCalculator;
 import lotto.ui.InputView;
 import lotto.ui.OutputView;
@@ -8,19 +14,28 @@ import lotto.ui.OutputView;
 public class LottoController {
     private InputView inputView;
     private OutputView outputView;
-    private WinningCalculator winningCalculator;
 
     public LottoController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
-        outputView.announceInputTargetNumbers();
-        outputView.announceInputBonusNumber();
-        String targetNumbersRaw = inputView.getTargerNumbersFromUser();
-        String bonusNumberRaw = inputView.getBonusNumberFromUser();
-        this.winningCalculator = new WinningCalculator(targetNumbersRaw, bonusNumberRaw);
     }
 
-    public void purchaseLottos() {
+    public void start() {
+        List<Lotto> lottos = purchaseLottos();
+//        ResultTable resultTable = calculateResult(lottos);
+    }
 
+    private List<Lotto> purchaseLottos() {
+        List<Lotto> lottos = new ArrayList<>();
+        outputView.announceInputMoney();
+        int purchaseCount = Cashier.calculatePurchaseCountByMoney(inputView.getMoneyByUser());
+        outputView.announcePurchaseCount(purchaseCount);
+        IntStream.range(0, purchaseCount)
+                .forEach(number -> {
+                    List<Integer> numbers = NumbersGenerator.generateNumbers();
+                    outputView.printNumbers(numbers);
+                    lottos.add(new Lotto(numbers));
+                });
+        return lottos;
     }
 }
