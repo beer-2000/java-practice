f08308abcf9c40d62988e53d98d5de7ffedfdd17
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:06:52 +0900

    feat(InputView): 구입금액 입력 받기

--- wooteco/java-lotto/src/main/java/lotto/ui/InputView.java
+++ wooteco/java-lotto/src/main/java/lotto/ui/InputView.java
@@ -1,4 +1,10 @@
 package lotto.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    
+    public String getMoneyByUser() {
+        return Console.readLine();
+    }
 }
