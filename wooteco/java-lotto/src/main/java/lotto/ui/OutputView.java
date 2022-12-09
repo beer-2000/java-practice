c957b74967145f8cd75520f0513fe8b1a705e2b1
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:09:40 +0900

    feat(OutputView): 구매 개수 출력

--- wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
+++ wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
@@ -2,8 +2,13 @@ package lotto.ui;
 
 public class OutputView {
     private String REQUEST_INPUT_AMOUNT = "구입금액을 입력해 주세요.";
+    private String ANNOUNCEMENT_PURCHASE_COUNT = "%d개를 구매했습니다.";
 
     public void announceInputMoney() {
         System.out.println(REQUEST_INPUT_AMOUNT);
+    }
+
+    public void announcePurchaseCount(int purchaseCount) {
+        System.out.printf(ANNOUNCEMENT_PURCHASE_COUNT, purchaseCount);
     }
 }
