7bb75f8a209f2028497ca399bbd54e2be7536309
Woojin Lee <dhkep03@gmail.com>
2022-12-09 17:01:53 +0900

    refactor(OutputView): 메서드 이름 올바르게 수정

--- wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
+++ wooteco/java-lotto/src/main/java/lotto/ui/OutputView.java
@@ -3,7 +3,7 @@ package lotto.ui;
 public class OutputView {
     private String REQUEST_INPUT_AMOUNT = "구입금액을 입력해 주세요.";
 
-    public void announceInputTargetNumbers() {
+    public void announceInputMoney() {
         System.out.println(REQUEST_INPUT_AMOUNT);
     }
 }
