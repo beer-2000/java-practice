dbe56d87b042ee7b787f22096925c2bd03e259a3
Woojin Lee <dhkep03@gmail.com>
2022-12-09 18:17:28 +0900

    feat(NumberGenerator): 로또 숫자 정렬 적용

--- wooteco/java-lotto/src/main/java/lotto/domain/NumbersGenerator.java
+++ wooteco/java-lotto/src/main/java/lotto/domain/NumbersGenerator.java
@@ -6,12 +6,15 @@ import static lotto.constant.LottoRule.MINIMUM_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NumbersGenerator {
     public static List<Integer> generateNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 MINIMUM_NUMBER, MAXIMUM_NUMBER, COUNT_OF_NUMBERS
         );
-        return numbers;
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
