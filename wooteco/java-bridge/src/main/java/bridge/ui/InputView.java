805e63b79274f9ca136178319952f3a4839ae775
Woojin Lee <dhkep03@gmail.com>
2022-12-10 14:57:22 +0900

    feat(InputView): 게임 재시작 여부 입력

--- wooteco/java-bridge/src/main/java/bridge/ui/InputView.java
+++ wooteco/java-bridge/src/main/java/bridge/ui/InputView.java
@@ -7,25 +7,16 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
         String bridgeSizeRaw = Console.readLine();
         return Integer.parseInt(bridgeSizeRaw);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
         return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        return Console.readLine();
     }
 }
