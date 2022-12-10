e574bd09f00b16d9a2b61a2d728cbf74b26a4d34
Woojin Lee <dhkep03@gmail.com>
2022-12-10 23:01:26 +0900

    feat(RacingGame): 자동차 등록

--- wooteco/java-racingcar/src/main/java/racingcar/domain/RacingGame.java
+++ wooteco/java-racingcar/src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,16 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+    private List<Car> cars;
+
+    public RacingGame(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        carNames.stream().forEach(carName -> {
+            cars.add(new Car(carName));
+        });
+        this.cars = cars;
+    }
 }
