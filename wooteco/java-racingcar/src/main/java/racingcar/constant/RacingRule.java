6cfb2acec52d78879e5539023c7be9dec01592ac
Woojin Lee <dhkep03@gmail.com>
2022-12-10 23:24:41 +0900

    feat(Car): 이동

--- wooteco/java-racingcar/src/main/java/racingcar/constant/RacingRule.java
+++ wooteco/java-racingcar/src/main/java/racingcar/constant/RacingRule.java
@@ -4,4 +4,5 @@ public class RacingRule {
     public static final int RANDOM_START_INCLUSIVE = 0;
     public static final int RANDOM_END_INCLUSIVE = 9;
     public static final int MOVING_THRESHOLD = 4;
+    public static final int MOVING_DISTANCE = 1;
 }
--- wooteco/java-racingcar/src/main/java/racingcar/domain/Car.java
+++ wooteco/java-racingcar/src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,12 @@
 package racingcar.domain;
 
+import static racingcar.constant.RacingRule.MOVING_DISTANCE;
+import static racingcar.constant.RacingRule.MOVING_THRESHOLD;
+import static racingcar.constant.RacingRule.RANDOM_END_INCLUSIVE;
+import static racingcar.constant.RacingRule.RANDOM_START_INCLUSIVE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +15,10 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(RANDOM_START_INCLUSIVE, RANDOM_END_INCLUSIVE);
+        if (randomNumber >= MOVING_THRESHOLD) {
+            position += MOVING_DISTANCE;
+        }
+    }
 }
