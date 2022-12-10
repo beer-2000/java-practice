6c95e61396368c23a9a778ac1be7456112bedf7b
Woojin Lee <dhkep03@gmail.com>
2022-12-10 14:28:29 +0900

    feat(InputView): 다리 길이 입력

--- wooteco/java-bridge/src/main/java/bridge/ui/InputView.java
+++ wooteco/java-bridge/src/main/java/bridge/ui/InputView.java
@@ -1,5 +1,7 @@
 package bridge.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSizeRaw = Console.readLine();
+        return Integer.parseInt(bridgeSizeRaw);
     }
 
     /**
