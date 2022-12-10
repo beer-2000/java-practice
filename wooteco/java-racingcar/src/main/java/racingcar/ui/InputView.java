39ec79bfb6a7844334fb038ba767c3558c344cab
Woojin Lee <dhkep03@gmail.com>
2022-12-10 22:57:38 +0900

    feat(InputView): 자동차 이름 입력

--- wooteco/java-racingcar/src/main/java/racingcar/ui/InputView.java
+++ wooteco/java-racingcar/src/main/java/racingcar/ui/InputView.java
@@ -1,4 +1,15 @@
 package racingcar.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+    public List<String> readCarNames() {
+        String carNamesRaw = Console.readLine();
+        return Arrays.stream(carNamesRaw.split(","))
+                .collect(Collectors.toList());
+
+    }
 }
