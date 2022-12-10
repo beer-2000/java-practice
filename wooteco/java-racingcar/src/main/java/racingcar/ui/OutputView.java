aeef6ce3c93d6516613b11abe7178bd07e5cb663
Woojin Lee <dhkep03@gmail.com>
2022-12-10 23:12:16 +0900

    feat(OutputView): 실행 결과 안내 멘트 출력

--- wooteco/java-racingcar/src/main/java/racingcar/ui/OutputView.java
+++ wooteco/java-racingcar/src/main/java/racingcar/ui/OutputView.java
@@ -3,6 +3,7 @@ package racingcar.ui;
 public class OutputView {
     private final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String REQUEST_RACING_TIMES = "시도할 회수는 몇회인가요?";
+    private final String ANNOUNCEMENT_PLAY_RESULT = "실행 결과";
 
     public void requestCarNames() {
         System.out.println(REQUEST_CAR_NAMES);
@@ -10,5 +11,10 @@ public class OutputView {
 
     public void requestRacingTimes() {
         System.out.println(REQUEST_RACING_TIMES);
+    }
+
+    public void announcePlayResult() {
+        System.out.println();
+        System.out.println(ANNOUNCEMENT_PLAY_RESULT);
     }
 }
