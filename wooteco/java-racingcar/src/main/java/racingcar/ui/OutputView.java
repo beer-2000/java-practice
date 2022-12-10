e30efdcd5f0fe957f201ebad7702ef8b0db0adae
Woojin Lee <dhkep03@gmail.com>
2022-12-10 23:10:45 +0900

    feat(OutputView): 시도 횟수 입력 요청 멘트 출력

--- wooteco/java-racingcar/src/main/java/racingcar/ui/OutputView.java
+++ wooteco/java-racingcar/src/main/java/racingcar/ui/OutputView.java
@@ -2,8 +2,13 @@ package racingcar.ui;
 
 public class OutputView {
     private final String REQUEST_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String REQUEST_RACING_TIMES = "시도할 회수는 몇회인가요?";
 
     public void requestCarNames() {
         System.out.println(REQUEST_CAR_NAMES);
+    }
+
+    public void requestRacingTimes() {
+        System.out.println(REQUEST_RACING_TIMES);
     }
 }
