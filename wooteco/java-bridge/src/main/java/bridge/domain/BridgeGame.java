8ca3eb23d59a4d304d0e5b8b683535ca8e9905bd
Woojin Lee <dhkep03@gmail.com>
2022-12-10 15:23:28 +0900

    feat(BridgeGame): 게임 재시작

--- wooteco/java-bridge/src/main/java/bridge/domain/BridgeGame.java
+++ wooteco/java-bridge/src/main/java/bridge/domain/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import static bridge.constant.BridgeRule.COMMAND_QUIT;
+import static bridge.constant.ProgressStatus.FAIL;
 import static bridge.constant.ProgressStatus.ON_WAY;
 import static bridge.constant.ProgressStatus.SUCCESS;
 
@@ -44,7 +46,13 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(String gameCommand) {
+        if (gameCommand.equals(COMMAND_QUIT)) {
+            progressStatus = FAIL;
+            return;
+        }
+        this.movingHistory = new ArrayList<>();
+        tryCount += 1;
     }
 
     public boolean isOnWay() {
