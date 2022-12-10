d256f9949c36066087fa96a424b9f84eeb530b99
Woojin Lee <dhkep03@gmail.com>
2022-12-10 14:07:21 +0900

    feat: 프로그램 시작 로직

--- wooteco/java-bridge/src/main/java/bridge/controller/BridgeController.java
+++ wooteco/java-bridge/src/main/java/bridge/controller/BridgeController.java
@@ -1,4 +1,16 @@
 package bridge.controller;
 
+import bridge.domain.BridgeGame;
+import bridge.ui.InputView;
+import bridge.ui.OutputView;
+
 public class BridgeController {
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeGame bridgeGame;
+
+    public BridgeController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
 }
