fdd9d6a5e9c08c3bb49c119744f82836cdb1fd00
Woojin Lee <dhkep03@gmail.com>
2022-12-10 13:59:44 +0900

    feat(BridgeRule): 상수 저장

--- wooteco/java-bridge/src/main/java/bridge/constant/BridgeRule.java
+++ wooteco/java-bridge/src/main/java/bridge/constant/BridgeRule.java
@@ -1,4 +1,8 @@
 package bridge.constant;
 
 public class BridgeRule {
+    public final String UPPER_SIDE = "U";
+    public final String LOWER_SIDE = "D";
+    public final String COMMAND_RETRY = "R";
+    public final String COMMAND_QUIT = "Q";
 }
