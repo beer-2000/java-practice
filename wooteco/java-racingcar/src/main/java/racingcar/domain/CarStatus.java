a7f65e683b318fa33506fc7f0ab2bea9888bb39a
Woojin Lee <dhkep03@gmail.com>
2022-12-10 23:17:39 +0900

    feat(CarStatus): Value Object 구현

--- wooteco/java-racingcar/src/main/java/racingcar/domain/CarStatus.java
+++ wooteco/java-racingcar/src/main/java/racingcar/domain/CarStatus.java
@@ -1,4 +1,39 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class CarStatus {
+    private final String name;
+    private final int position;
+
+    public CarStatus(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final CarStatus carStatus = (CarStatus) o;
+        return name == carStatus.name &&
+                position == carStatus.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
 }
