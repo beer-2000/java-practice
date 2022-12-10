978b596f120ef7ebc39e98ddffb94edb86b60103
Woojin Lee <dhkep03@gmail.com>
2022-12-10 14:21:20 +0900

    feat(BridgeController): 다리 게임 생성 로직 구현

--- wooteco/java-bridge/src/main/java/bridge/controller/BridgeController.java
+++ wooteco/java-bridge/src/main/java/bridge/controller/BridgeController.java
@@ -1,8 +1,11 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
 import bridge.domain.BridgeGame;
+import bridge.domain.MovingStatus;
 import bridge.ui.InputView;
 import bridge.ui.OutputView;
+import java.util.List;
 
 public class BridgeController {
     private InputView inputView;
@@ -12,5 +15,17 @@ public class BridgeController {
     public BridgeController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+    }
+
+    public void start() {
+        generateBridgeGame();
+    }
+
+    private void generateBridgeGame() {
+        outputView.announceStartGame();
+        outputView.requestBridgeSize();
+        BridgeMaker bridgeMaker = new BridgeMaker();
+        List<String> bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+        this.bridgeGame = new BridgeGame(bridge);
     }
 }
