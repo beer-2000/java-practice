83c26b5d7eff6a3a903c36cbd17700d8fe809fe1
Woojin Lee <dhkep03@gmail.com>
2022-12-11 18:06:42 +0900

    feat(OutputView): 카드 분배 안내 멘트 출력

--- wooteco/java-blackjack/src/main/java/ui/OutputView.java
+++ wooteco/java-blackjack/src/main/java/ui/OutputView.java
@@ -1,8 +1,11 @@
 package ui;
 
+import java.util.List;
+
 public class OutputView {
     private final String REQUEST_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private final String REQUEST_BETTING_MONEY = "%s의 배팅 금액은?";
+    private final String ANNOUNCEMENT_DISTRIBUTION = "딜러와 %s에게 2장을 나누었습니다.";
 
     public void requestPlayerNames() {
         System.out.println(REQUEST_PLAYER_NAMES);
@@ -11,6 +14,13 @@ public class OutputView {
     public void requestBettingMoney(String name) {
         System.out.println();
         System.out.printf(REQUEST_BETTING_MONEY, name);
+        System.out.println();
+    }
+
+    public void announceDistribution(List<String> playerNames) {
+        System.out.println();
+        String playerNamesJoined = String.join(", ", playerNames);
+        System.out.printf(ANNOUNCEMENT_DISTRIBUTION, playerNamesJoined);
         System.out.println();
     }
 }
