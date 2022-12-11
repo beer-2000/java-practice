3b727650292a3947f8a96d32fcdc1e0638712ec2
Woojin Lee <dhkep03@gmail.com>
2022-12-11 19:38:39 +0900

    feat(OutputView): 추가 카드 발급 여부 입력 요청 멘트 출력

--- wooteco/java-blackjack/src/main/java/ui/OutputView.java
+++ wooteco/java-blackjack/src/main/java/ui/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private final String REQUEST_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private final String REQUEST_BETTING_MONEY = "%s의 배팅 금액은?";
     private final String ANNOUNCEMENT_DISTRIBUTION = "딜러와 %s에게 2장을 나누었습니다.";
+    private final String REQUEST_COMMAND_GET_CARD = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
 
     public void requestPlayerNames() {
         System.out.println(REQUEST_PLAYER_NAMES);
@@ -26,5 +27,11 @@ public class OutputView {
 
     public void printCards(String cardValues) {
         System.out.println(cardValues);
+    }
+
+    public void requestWhetherGetCard(String name) {
+        System.out.println();
+        System.out.printf(REQUEST_COMMAND_GET_CARD, name);
+        System.out.println();
     }
 }
