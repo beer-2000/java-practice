1bc5a5b1c1985aa60eba0a11b6bba2f997b25fa3
Woojin Lee <dhkep03@gmail.com>
2022-12-11 17:41:12 +0900

    feat(OutputView): 플레이어 이름 목록 입력 요청 멘트 출력

--- wooteco/java-blackjack/src/main/java/ui/OutputView.java
+++ wooteco/java-blackjack/src/main/java/ui/OutputView.java
@@ -1,4 +1,9 @@
 package ui;
 
 public class OutputView {
+    private final String REQUEST_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+
+    public void requestPlayerNames() {
+        System.out.println(REQUEST_PLAYER_NAMES);
+    }
 }
