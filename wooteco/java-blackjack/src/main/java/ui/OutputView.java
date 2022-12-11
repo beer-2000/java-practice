82c6824ab92091d5e71d01cfd15558d21f057268
Woojin Lee <dhkep03@gmail.com>
2022-12-11 17:46:09 +0900

    feat(OutputView): 플레이어 베팅 금액 입력 요청 멘트 출력

--- wooteco/java-blackjack/src/main/java/ui/OutputView.java
+++ wooteco/java-blackjack/src/main/java/ui/OutputView.java
@@ -2,8 +2,13 @@ package ui;
 
 public class OutputView {
     private final String REQUEST_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private final String REQUEST_BETTING_MONEY = "%s의 배팅 금액은?";
 
     public void requestPlayerNames() {
         System.out.println(REQUEST_PLAYER_NAMES);
+    }
+
+    public void requestBettingMoney(String name) {
+        System.out.printf(REQUEST_BETTING_MONEY, name);
     }
 }
