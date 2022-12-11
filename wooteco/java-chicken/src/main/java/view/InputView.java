19912fa5796e7d0d150eceddda7313e06ac45ac8
Woojin Lee <dhkep03@gmail.com>
2022-12-11 23:55:46 +0900

    refactor(InputView): 줄띄움 추가

--- wooteco/java-chicken/src/main/java/view/InputView.java
+++ wooteco/java-chicken/src/main/java/view/InputView.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String REQUEST_TABLE_NUMBER = "## 주문할 테이블을 선택하세요.";
-    private static final String REQUEST_FUNCTION = "## 원하는 기능을 선택하세요.";
-    private static final String REQUEST_MENU_NUMBER = "## 등록할 메뉴를 선택하세요.";
-    private static final String REQUEST_MENU_COUNT = "## 메뉴의 수량을 입력하세요.";
+    private static final String REQUEST_TABLE_NUMBER = System.lineSeparator() + "## 주문할 테이블을 선택하세요.";
+    private static final String REQUEST_FUNCTION = System.lineSeparator() + "## 원하는 기능을 선택하세요.";
+    private static final String REQUEST_MENU_NUMBER = System.lineSeparator() + "## 등록할 메뉴를 선택하세요.";
+    private static final String REQUEST_MENU_COUNT = System.lineSeparator() + "## 메뉴의 수량을 입력하세요.";
 
     public static int inputTableNumber() {
         System.out.println(REQUEST_TABLE_NUMBER);
