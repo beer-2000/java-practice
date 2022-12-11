60940595003037e1d424c59f7667ab1bf7d2da1c
Woojin Lee <dhkep03@gmail.com>
2022-12-11 17:41:34 +0900

    feat(BlackjackController): 플레이어 생성

--- wooteco/java-blackjack/src/main/java/controller/BlackjackController.java
+++ wooteco/java-blackjack/src/main/java/controller/BlackjackController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import domain.Blackjack;
+import domain.user.Dealer;
+import domain.user.Player;
+import java.util.List;
 import ui.InputView;
 import ui.OutputView;
 
@@ -8,14 +11,27 @@ public class BlackjackController {
     private final InputView inputView;
     private final OutputView outputView;
     private final Blackjack blackjack;
+    private Dealer dealer;
+    private List<Player> players;
 
     public BlackjackController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.blackjack = new Blackjack();
+        this.dealer = new Dealer();
     }
 
     public void start() {
-        
+        generatePlayers();
+    }
+
+    private void generatePlayers() {
+        outputView.requestPlayerNames();
+        List<String> playerNames = inputView.readPlayerNames();
+        playerNames.stream().forEach(name -> {
+            outputView.requestBettingMoney(name);
+            double bettingMoney = inputView.readBettingMoney();
+            players.add(new Player(name, bettingMoney));
+        });
     }
 }
--- wooteco/java-blackjack/src/main/java/domain/Blackjack.java
+++ wooteco/java-blackjack/src/main/java/domain/Blackjack.java
@@ -1,4 +1,16 @@
 package domain;
 
+import domain.card.Card;
+import domain.card.CardFactory;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Blackjack {
+    private List<Card> cardDummy;
+    private List<Card> usedCards;
+
+    public Blackjack() {
+        this.cardDummy = CardFactory.create();
+        usedCards = new ArrayList<>();
+    }
 }
