53d72622df594ecb454386a5fcdb195d3c5c6384
Woojin Lee <dhkep03@gmail.com>
2022-12-11 18:11:13 +0900

    feat(Blackjack): 중복되지 않은 카드 발급

--- wooteco/java-blackjack/src/main/java/domain/Blackjack.java
+++ wooteco/java-blackjack/src/main/java/domain/Blackjack.java
@@ -4,6 +4,7 @@ import domain.card.Card;
 import domain.card.CardFactory;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Blackjack {
     private List<Card> cardDummy;
@@ -12,5 +13,15 @@ public class Blackjack {
     public Blackjack() {
         this.cardDummy = CardFactory.create();
         usedCards = new ArrayList<>();
+    }
+
+    public Card getNewCard() {
+        Random random = new Random();
+        Card card;
+        do {
+            card = cardDummy.get(random.nextInt(52));
+        } while (usedCards.contains(card));
+        usedCards.add(card);
+        return card;
     }
 }
