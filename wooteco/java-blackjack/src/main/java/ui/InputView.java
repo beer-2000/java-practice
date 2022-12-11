41e903f65109e5f59ba2307a14da92661129b8e4
Woojin Lee <dhkep03@gmail.com>
2022-12-11 17:44:43 +0900

    feat(InputView): 플레이어 이름 목록 입력

--- wooteco/java-blackjack/src/main/java/ui/InputView.java
+++ wooteco/java-blackjack/src/main/java/ui/InputView.java
@@ -1,4 +1,15 @@
 package ui;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class InputView {
+    public List<String> readPlayerNames() {
+        Scanner scanner = new Scanner(System.in);
+        String playerNamesRaw = scanner.nextLine();
+        return Arrays.stream(playerNamesRaw.split(","))
+                .collect(Collectors.toList());
+    }
 }
