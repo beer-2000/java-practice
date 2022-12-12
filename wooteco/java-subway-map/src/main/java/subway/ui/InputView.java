3f4836ba9b946c953a05ccbbc6dfcb5dcd3986db
Woojin Lee <dhkep03@gmail.com>
2022-12-12 22:58:38 +0900

    feat(InputView): 메인 메뉴 입력

--- wooteco/java-subway-map/src/main/java/subway/ui/InputView.java
+++ wooteco/java-subway-map/src/main/java/subway/ui/InputView.java
@@ -1,4 +1,14 @@
 package subway.ui;
 
+import java.util.Scanner;
+import subway.constant.FunctionCommand;
+
 public class InputView {
+    private Scanner scanner;
+
+    public FunctionCommand readFunctionCommand() {
+        scanner = new Scanner(System.in);
+        String command = scanner.next();
+        return FunctionCommand.get(command);
+    }
 }
