d4f6dbb56a452dbb224e93410e1a18234eb16efb
Woojin Lee <dhkep03@gmail.com>
2022-12-12 23:11:23 +0900

    feat(OutputView): 역 기능 목록 출력

--- wooteco/java-subway-map/src/main/java/subway/constant/StationCommand.java
+++ wooteco/java-subway-map/src/main/java/subway/constant/StationCommand.java
@@ -1,5 +1,7 @@
 package subway.constant;
 
+import java.util.Arrays;
+
 public enum StationCommand {
     REGISTER("1", "1. 역 등록"),
     DELETE("2", "2. 역 삭제"),
@@ -8,9 +10,25 @@ public enum StationCommand {
 
     private String command;
     private String message;
+    private static final String ERROR_MESSAGE_WRONG_FUNCTION_COMMAND = "[ERROR] 선택할 수 없는 기능입니다.";
 
     StationCommand(String command, String message) {
         this.command = command;
         this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static StationCommand get(String command) {
+        return Arrays.stream(StationCommand.values())
+                .filter(value -> value.isCommandOf(command))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_WRONG_FUNCTION_COMMAND));
+    }
+
+    private boolean isCommandOf(String command) {
+        return this.command.equals(command);
     }
 }
--- wooteco/java-subway-map/src/main/java/subway/ui/OutputView.java
+++ wooteco/java-subway-map/src/main/java/subway/ui/OutputView.java
@@ -2,13 +2,23 @@ package subway.ui;
 
 import java.util.Arrays;
 import subway.constant.FunctionCommand;
+import subway.constant.StationCommand;
 
 public class OutputView {
     private final String ANNOUNCEMENT_MAIN_FUNCTION = "## 메인 화면";
+    private final String ANNOUNCEMENT_STATION_FUNCTION = "## 역 관리 화면";
 
     public void announceMainFunction() {
         System.out.println(ANNOUNCEMENT_MAIN_FUNCTION);
         Arrays.stream(FunctionCommand.values())
+                .forEach(value -> {
+                    System.out.println(value.getMessage());
+                });
+    }
+
+    public void announceStationFunction() {
+        System.out.println(ANNOUNCEMENT_STATION_FUNCTION);
+        Arrays.stream(StationCommand.values())
                 .forEach(value -> {
                     System.out.println(value.getMessage());
                 });
