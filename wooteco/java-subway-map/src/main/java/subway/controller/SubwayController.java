8f178a82d0e44729e7f12bd77f1cf886a64e0986
Woojin Lee <dhkep03@gmail.com>
2022-12-12 21:48:53 +0900

    feat(SubwayController, Appliction): 시작 로직 설정

--- wooteco/java-subway-map/src/main/java/subway/controller/SubwayController.java
+++ wooteco/java-subway-map/src/main/java/subway/controller/SubwayController.java
@@ -1,4 +1,24 @@
 package subway.controller;
 
+import subway.domain.Lines;
+import subway.domain.Stations;
+import subway.ui.InputView;
+import subway.ui.OutputView;
+
 public class SubwayController {
+    private InputView inputView;
+    private OutputView outputView;
+    private Stations stations;
+    private Lines lines;
+
+    public SubwayController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.stations = new Stations();
+        this.lines = new Lines();
+    }
+
+    public void start() {
+        
+    }
 }
