3cfa99e10e08541c1e08a78f4318ffe1e7380a37
Woojin Lee <dhkep03@gmail.com>
2022-12-12 21:52:59 +0900

    feat(Stations): 생성

--- wooteco/java-subway-map/src/main/java/subway/domain/Stations.java
+++ wooteco/java-subway-map/src/main/java/subway/domain/Stations.java
@@ -1,4 +1,19 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stations {
+    private List<Station> stations;
+
+    public Stations() {
+        this.stations = new ArrayList<>();
+        stations.add(new Station("교대역"));
+        stations.add(new Station("강남역"));
+        stations.add(new Station("역삼역"));
+        stations.add(new Station("남부터미널역"));
+        stations.add(new Station("양재역"));
+        stations.add(new Station("양재시민의숲역"));
+        stations.add(new Station("매봉역"));
+    }
 }
