00b6fcc9c9bec40f711a21fbc85e98a0cd422c73
Woojin Lee <dhkep03@gmail.com>
2022-12-12 23:01:34 +0900

    refactor(InputView): 안내 메시지 추가

--- wooteco/java-subway-map/src/main/java/subway/ui/InputView.java
+++ wooteco/java-subway-map/src/main/java/subway/ui/InputView.java
@@ -5,8 +5,11 @@ import subway.constant.FunctionCommand;
 
 public class InputView {
     private Scanner scanner;
+    private final String REQUEST_COMMAND = "## 원하는 기능을 선택하세요.";
 
     public FunctionCommand readFunctionCommand() {
+        System.out.println();
+        System.out.println(REQUEST_COMMAND);
         scanner = new Scanner(System.in);
         String command = scanner.next();
         return FunctionCommand.get(command);
