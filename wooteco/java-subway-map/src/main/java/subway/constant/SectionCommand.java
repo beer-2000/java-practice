9dc1481e75992670e0aa84df978d77eb51634fb0
Woojin Lee <dhkep03@gmail.com>
2022-12-12 21:46:44 +0900

    feat(SectionCommand): 구현

--- wooteco/java-subway-map/src/main/java/subway/constant/SectionCommand.java
+++ wooteco/java-subway-map/src/main/java/subway/constant/SectionCommand.java
@@ -1,4 +1,15 @@
 package subway.constant;
 
 public enum SectionCommand {
+    REGISTER("1", "1. 구간 등록"),
+    DELETE("2", "2. 구간 삭제"),
+    BACK("B", "B. 돌아가기");
+
+    private String command;
+    private String message;
+
+    SectionCommand(String command, String message) {
+        this.command = command;
+        this.message = message;
+    }
 }
