67f1a4d922e0afdc86a73294a23bc02573775434
Woojin Lee <dhkep03@gmail.com>
2022-12-13 14:48:01 +0900

    refactor: 노선 이름 중복 금지

--- wooteco/java-subway-map/src/main/java/subway/domain/Lines.java
+++ wooteco/java-subway-map/src/main/java/subway/domain/Lines.java
@@ -7,12 +7,14 @@ import java.util.stream.Collectors;
 public class Lines {
     private List<Line> lines;
     private final String ERROR_MESSAGE_NOT_EXIST_LINE_NAME = "[ERROR] 존재하지 않는 노선 이름입니다.";
+    private final String ERROR_MESSAGE_ALREADY_EXIST_LINE_NAME = "[ERROR] 이미 존재하는 노선 이름입니다.";
 
     public Lines() {
         lines = new ArrayList<>();
     }
 
     public void addLine(String name, Station startStation, Station endStation) {
+        validateDuplication(name);
         lines.add(new Line(name, startStation, endStation));
     }
 
@@ -41,5 +43,13 @@ public class Lines {
         return lines.stream()
                 .map(Line::getLineMap)
                 .collect(Collectors.toList());
+    }
+
+    private void validateDuplication(String name) {
+        for (Line line : lines) {
+            if (line.getName().equals(name)) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_ALREADY_EXIST_LINE_NAME);
+            }
+        }
     }
 }
