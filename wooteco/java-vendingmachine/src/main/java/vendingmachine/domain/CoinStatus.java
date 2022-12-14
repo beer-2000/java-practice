d0b40a792eb89189aabc5462f542a2b05c79a81c
Woojin Lee <dhkep03@gmail.com>
2022-12-14 13:03:13 +0900

    feat(CointStatus): 10원 여부에 따라 다르게 랜덤 개수 생성

--- wooteco/java-vendingmachine/src/main/java/vendingmachine/domain/CoinStatus.java
+++ wooteco/java-vendingmachine/src/main/java/vendingmachine/domain/CoinStatus.java
@@ -1,5 +1,7 @@
 package vendingmachine.domain;
 
+import static vendingmachine.domain.Coin.COIN_10;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +13,29 @@ public class CoinStatus {
 
     public CoinStatus(Coin coin, int money) {
         this.coin = coin;
+        generateCount(coin, money);
+    }
+
+    private void generateCount(Coin coin, int money) {
+        if (coin.equals(COIN_10)) {
+            generateCountOf10(coin, money);
+            return;
+        }
+        generateCountExcept10(coin, money);
+    }
+
+    private void generateCountExcept10(Coin coin, int money) {
         int amount = coin.getAmount();
         int maxCount = money / amount;
         List<Integer> randomCounts = new ArrayList<>();
         IntStream.rangeClosed(0, maxCount)
                 .forEach(number -> randomCounts.add(number));
         this.count = Randoms.pickNumberInList(randomCounts);
+    }
+
+    private void generateCountOf10(Coin coin, int money) {
+        int amount = coin.getAmount();
+        this.count = money / amount;
     }
 
     public boolean isOf(Coin coin) {
