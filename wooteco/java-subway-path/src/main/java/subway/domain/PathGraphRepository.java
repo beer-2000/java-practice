79c08398fa3a0f0d703bac70d12f05dff9f65f49
Woojin Lee <dhkep03@gmail.com>
2022-12-14 23:48:35 +0900

    feat(PathGraphRepository): 경로 그래프 저장

--- wooteco/java-subway-path/src/main/java/subway/domain/PathGraphRepository.java
+++ wooteco/java-subway-path/src/main/java/subway/domain/PathGraphRepository.java
@@ -1,4 +1,12 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PathGraphRepository {
+    private static final List<PathGraph> pathGraphs = new ArrayList<>();
+
+    public static void addPathGraph(PathGraph pathGraph) {
+        pathGraphs.add(pathGraph);
+    }
 }
