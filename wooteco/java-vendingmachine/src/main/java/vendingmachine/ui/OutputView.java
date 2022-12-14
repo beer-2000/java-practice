12d17c188218b689be5bbfa4e385d98da960297e
Woojin Lee <dhkep03@gmail.com>
2022-12-14 13:21:54 +0900

    feat(OutputView): 자판기 초기 보유 동전 현황 출력

--- wooteco/java-vendingmachine/src/main/java/vendingmachine/ui/OutputView.java
+++ wooteco/java-vendingmachine/src/main/java/vendingmachine/ui/OutputView.java
@@ -1,7 +1,21 @@
 package vendingmachine.ui;
 
+import java.util.List;
+import vendingmachine.domain.CoinInfo;
+
 public class OutputView {
+    private final String ANNOUNCEMENT_COIN_INFO = System.lineSeparator() + "자판기가 보유한 동전";
+    private final String COIN_PRINT_FORMAT = "%s - %s개";
+
     public void printErrorMessage(String errorMessage) {
         System.out.println(errorMessage);
+    }
+
+    public void printCoinCount(List<CoinInfo> coinInfos) {
+        System.out.println(ANNOUNCEMENT_COIN_INFO);
+        coinInfos.forEach(coinInfo -> {
+            System.out.printf(COIN_PRINT_FORMAT, coinInfo.getCoinName(), coinInfo.getCount());
+            System.out.println();
+        });
     }
 }
