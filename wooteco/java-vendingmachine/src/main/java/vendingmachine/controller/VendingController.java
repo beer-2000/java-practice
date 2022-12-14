6b3162eace730181784a02923ebeb7ff4f3e476e
Woojin Lee <dhkep03@gmail.com>
2022-12-14 12:23:30 +0900

    feat(VendingController): 자판기 생성

--- wooteco/java-vendingmachine/src/main/java/vendingmachine/controller/VendingController.java
+++ wooteco/java-vendingmachine/src/main/java/vendingmachine/controller/VendingController.java
@@ -1,4 +1,25 @@
 package vendingmachine.controller;
 
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.ui.InputView;
+import vendingmachine.ui.OutputView;
+
 public class VendingController {
+    private InputView inputView;
+    private OutputView outputView;
+    private VendingMachine vendingMachine;
+
+    public VendingController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start() {
+        generateVendingMachine();
+    }
+
+    private void generateVendingMachine() {
+        int moneyOfVendingMachine = inputView.readMoneyOfVendingMachine();
+        this.vendingMachine = new VendingMachine(moneyOfVendingMachine);
+    }
 }
