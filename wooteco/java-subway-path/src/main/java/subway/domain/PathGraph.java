0ac4ff9047d2221ebccd37b34ef4a9673ed1dfd0
Woojin Lee <dhkep03@gmail.com>
2022-12-14 23:44:55 +0900

    feat(PathGraph): 생성

--- wooteco/java-subway-path/src/main/java/subway/domain/PathGraph.java
+++ wooteco/java-subway-path/src/main/java/subway/domain/PathGraph.java
@@ -1,4 +1,16 @@
 package subway.domain;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import subway.constant.PathCommand;
+
 public class PathGraph {
+    private PathCommand pathCommand;
+    private WeightedMultigraph graph;
+
+    public PathGraph(PathCommand pathCommand) {
+        this.pathCommand = pathCommand;
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        this.graph = graph;
+    }
 }
