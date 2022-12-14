e550ffcfe4df0fc235d916d374cf65ba8b1ab6a9
Woojin Lee <dhkep03@gmail.com>
2022-12-14 22:57:46 +0900

    feat(OutputView): 메인 화면 기능 목록 출력

--- wooteco/java-subway-path/src/main/java/subway/ui/OutputView.java
+++ wooteco/java-subway-path/src/main/java/subway/ui/OutputView.java
@@ -1,4 +1,15 @@
 package subway.ui;
 
+import java.util.Arrays;
+import subway.constant.MainCommand;
+
 public class OutputView {
+    private final String ANNOUNCE_MAIN_COMMAND = "## 메인 화면";
+
+    public void announceMainCommand() {
+        System.out.println(ANNOUNCE_MAIN_COMMAND);
+        Arrays.stream(MainCommand.values())
+                .forEach(mainCommand -> System.out.println(mainCommand.getMessage()));
+        System.out.println();
+    }
 }
