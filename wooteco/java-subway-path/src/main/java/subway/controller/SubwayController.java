3a7e4e9f8a2295cfcdb37b9b03e5ed9ae246604f
Woojin Lee <dhkep03@gmail.com>
2022-12-14 22:54:48 +0900

    feat: 프로그램 전체 실행 로직

--- wooteco/java-subway-path/src/main/java/subway/controller/SubwayController.java
+++ wooteco/java-subway-path/src/main/java/subway/controller/SubwayController.java
@@ -1,24 +1,54 @@
 package subway.controller;
 
 import java.util.Scanner;
-import subway.domain.LineRepository;
-import subway.domain.PathGraphRepository;
-import subway.domain.StationRepository;
+import subway.constant.MainCommand;
+import subway.constant.PathCommand;
+import subway.domain.Subway;
+import subway.ui.InputView;
+import subway.ui.OutputView;
 
 public class SubwayController {
     private Scanner scanner;
-    private StationRepository stationRepository;
-    private LineRepository lineRepository;
-    private PathGraphRepository pathGraphRepository;
+    private InputView inputView;
+    private OutputView outputView;
+    private Subway subway;
 
     public SubwayController(Scanner scanner) {
         this.scanner = scanner;
-        this.stationRepository = new StationRepository();
-        this.lineRepository = new LineRepository();
-        this.pathGraphRepository = new PathGraphRepository();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.subway = new Subway();
     }
 
     public void start() {
-        
+        MainCommand mainCommand = null;
+        do {
+            try {
+                outputView.announceMainCommand();
+                mainCommand = inputView.readMainCommand();
+                runMainFunction(mainCommand);
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        } while (mainCommand.equals(MainCommand.QUIT));
+    }
+
+    private void runMainFunction(MainCommand mainCommand) {
+        if (mainCommand.equals(MainCommand.PATH_FIND)) {
+            startPathFunction();
+        }
+    }
+
+    private void startPathFunction() {
+        PathCommand pathCommand = inputView.readPathCommand();
+        if (pathCommand.equals(PathCommand.MINIMUM_DISTANCE)) {
+
+        }
+        if (pathCommand.equals(PathCommand.MINIMUM_DISTANCE)) {
+
+        }
+        if (pathCommand.equals(PathCommand.MINIMUM_DISTANCE)) {
+
+        }
     }
 }
--- wooteco/java-subway-path/src/main/java/subway/domain/Subway.java
+++ wooteco/java-subway-path/src/main/java/subway/domain/Subway.java
@@ -1,4 +1,13 @@
 package subway.domain;
 
 public class Subway {
+    private StationRepository stationRepository;
+    private LineRepository lineRepository;
+    private PathGraphRepository pathGraphRepository;
+
+    public Subway() {
+        this.stationRepository = new StationRepository();
+        this.lineRepository = new LineRepository();
+        this.pathGraphRepository = new PathGraphRepository();
+    }
 }
