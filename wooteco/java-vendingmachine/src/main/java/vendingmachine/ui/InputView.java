52f96a56828c0ab56c6d395598c69fc50e53609b
Woojin Lee <dhkep03@gmail.com>
2022-12-14 14:18:31 +0900

    feat(InputView): 투입 금액 입력

--- wooteco/java-vendingmachine/src/main/java/vendingmachine/ui/InputView.java
+++ wooteco/java-vendingmachine/src/main/java/vendingmachine/ui/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
     private final String REQUEST_MONEY_OF_VENDING_MACHINE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private final String ERROR_MESSAGE_MONEY_SHOULD_BE_NUMBER = "[ERROR] 숫자를 입력해주세요.";
     private final String REQUEST_PRODUCT_INFO = "상품명과 가격, 수량을 입력해 주세요.";
+    private final String REQUEST_INPUT_MONEY = "투입 금액을 입력해 주세요.";
 
     public int readMoneyOfVendingMachine() {
         try {
@@ -23,5 +24,16 @@ public class InputView {
         String productInfosRaw = Console.readLine();
         System.out.println();
         return productInfosRaw;
+    }
+
+    public int readInputMoney() {
+        try {
+            System.out.println(REQUEST_INPUT_MONEY);
+            int money = Integer.parseInt(Console.readLine());
+            System.out.println();
+            return money;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_MONEY_SHOULD_BE_NUMBER);
+        }
     }
 }
