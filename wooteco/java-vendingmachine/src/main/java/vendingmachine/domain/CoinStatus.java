9a38808b07dfb8a243199b77ed43c24fbafa90f8
Woojin Lee <dhkep03@gmail.com>
2022-12-14 12:56:16 +0900

    feat(CoinStatus): 랜덤 개수 생성

--- wooteco/java-vendingmachine/src/main/java/vendingmachine/domain/CoinStatus.java
+++ wooteco/java-vendingmachine/src/main/java/vendingmachine/domain/CoinStatus.java
@@ -1,11 +1,22 @@
 package vendingmachine.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class CoinStatus {
     private Coin coin;
     private int count;
 
     public CoinStatus(Coin coin, int money) {
         this.coin = coin;
+        int amount = coin.getAmount();
+        int maxCount = money / amount;
+        List<Integer> randomCounts = new ArrayList<>();
+        IntStream.rangeClosed(0, maxCount)
+                .forEach(number -> randomCounts.add(number));
+        this.count = Randoms.pickNumberInList(randomCounts);
     }
 
     public boolean isOf(Coin coin) {
