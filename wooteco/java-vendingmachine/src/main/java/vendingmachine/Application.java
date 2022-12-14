c0b655b6b2094e01fe2814646d8f601f78151268
Woojin Lee <dhkep03@gmail.com>
2022-12-14 12:21:28 +0900

    feat(Application): 시작 로직 구현

--- wooteco/java-vendingmachine/src/main/java/vendingmachine/Application.java
+++ wooteco/java-vendingmachine/src/main/java/vendingmachine/Application.java
@@ -1,7 +1,10 @@
 package vendingmachine;
 
+import vendingmachine.controller.VendingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        VendingController vendingController = new VendingController();
+        vendingController.start();
     }
 }
