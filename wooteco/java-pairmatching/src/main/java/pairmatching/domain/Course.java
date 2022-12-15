93c7e89686aaec9eef8bed8f7e919872a5c8ccc2
Woojin Lee <dhkep03@gmail.com>
2022-12-15 19:31:44 +0900

    feat(Course): 생성

--- wooteco/java-pairmatching/src/main/java/pairmatching/domain/Course.java
+++ wooteco/java-pairmatching/src/main/java/pairmatching/domain/Course.java
@@ -1,4 +1,34 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Course {
+    private List<Level> levels;
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+        initLevels();
+    }
+
+    private void initLevels() {
+        this.levels = new ArrayList<>();
+        levels.add(new Level("레벨1"));
+        levels.add(new Level("레벨2"));
+        levels.add(new Level("레벨3"));
+        levels.add(new Level("레벨4"));
+        levels.add(new Level("레벨5"));
+        getLevelByName("레벨1").addMissions(Arrays.asList("자동차경주", "로또", "숫자야구게임"));
+        getLevelByName("레벨2").addMission(Arrays.asList("장바구니", "결제", "지하철노선도"));
+        getLevelByName("레벨4").addMission(Arrays.asList("성능개선", "배포"));
+    }
+
+    public Level getLevelByName(String name) {
+        return levels.stream()
+                .filter(level -> level.isNameOf(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 레벨 이름입니다."));
+    }
 }
