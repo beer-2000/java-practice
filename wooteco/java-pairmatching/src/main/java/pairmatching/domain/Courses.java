f050dc65528e54509ba29c6ff1d063e78bbf7f7a
Woojin Lee <dhkep03@gmail.com>
2022-12-15 19:21:40 +0900

    feat(Courses): 생성

--- wooteco/java-pairmatching/src/main/java/pairmatching/domain/Courses.java
+++ wooteco/java-pairmatching/src/main/java/pairmatching/domain/Courses.java
@@ -1,4 +1,14 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Courses {
+    private List<Course> courses;
+
+    public Courses() {
+        this.courses = new ArrayList<>();
+        courses.add(new Course("백엔드"));
+        courses.add(new Course("프론트엔드"));
+    }
 }
