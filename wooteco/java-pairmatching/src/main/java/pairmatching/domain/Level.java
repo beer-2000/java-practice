b799a157e0d270d59c20e90329ede59329df3bde
Woojin Lee <dhkep03@gmail.com>
2022-12-15 19:33:05 +0900

    feat(Level): 생성

--- wooteco/java-pairmatching/src/main/java/pairmatching/domain/Level.java
+++ wooteco/java-pairmatching/src/main/java/pairmatching/domain/Level.java
@@ -1,4 +1,14 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Level {
+    private List<Mission> missions;
+    private String name;
+
+    public Level(String name) {
+        this.name = name;
+        this.missions = new ArrayList<>();
+    }
 }
