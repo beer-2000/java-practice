4bd9d41a1f8eb1951ebbd3838de2521dafb4e41c
Woojin Lee <dhkep03@gmail.com>
2022-12-15 20:25:45 +0900

    feat(Members): 코스 구분에 따른 멤버 목록 생성

--- wooteco/java-pairmatching/src/main/java/pairmatching/domain/Members.java
+++ wooteco/java-pairmatching/src/main/java/pairmatching/domain/Members.java
@@ -1,16 +1,52 @@
 package pairmatching.domain;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Members {
     private List<Member> members;
 
     public Members(String courseName) {
+        this.members = new ArrayList<>();
         if (courseName.equals("백엔드")) {
             initMembersOfBackend();
         }
-        if (courseName.equals("프론트엔트")) {
+        if (courseName.equals("프론트엔드")) {
             initMembersOfFrontend();
+        }
+    }
+
+    private void initMembersOfBackend() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/backend-crew.md"));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                members.add(new Member(str));
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("[ERROR]: 파일을 찾을 수 없습니다.");
+        } catch (IOException e) {
+            throw new IllegalArgumentException("[ERROR]: IO에 문제가 있습니다.");
+        }
+    }
+
+    private void initMembersOfFrontend() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/frontend-crew.md"));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                members.add(new Member(str));
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("[ERROR]: 파일을 찾을 수 없습니다.");
+        } catch (IOException e) {
+            throw new IllegalArgumentException("[ERROR]: IO에 문제가 있습니다.");
         }
     }
 }
