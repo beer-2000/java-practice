a1ef837bd6869d2d6247ffd44bfadd07377ed127
Woojin Lee <dhkep03@gmail.com>
2022-12-15 19:16:22 +0900

    feat(InputView): 기능 번호 입력

--- wooteco/java-pairmatching/src/main/java/pairmatching/ui/InputView.java
+++ wooteco/java-pairmatching/src/main/java/pairmatching/ui/InputView.java
@@ -1,4 +1,15 @@
 package pairmatching.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import pairmatching.constant.FunctionCommand;
+
 public class InputView {
+
+    public FunctionCommand readFunctionCommand() {
+        System.out.println("기능을 선택하세요.");
+        Arrays.stream(FunctionCommand.values())
+                .forEach(functionCommand -> System.out.println(functionCommand.getMessage()));
+        return FunctionCommand.get(Console.readLine());
+    }
 }
