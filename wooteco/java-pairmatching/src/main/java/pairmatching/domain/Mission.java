23062f94f95ef6626499201618e68673ce807afe
Woojin Lee <dhkep03@gmail.com>
2022-12-15 19:37:40 +0900

    feat(Mission): 생성

--- wooteco/java-pairmatching/src/main/java/pairmatching/domain/Mission.java
+++ wooteco/java-pairmatching/src/main/java/pairmatching/domain/Mission.java
@@ -1,4 +1,13 @@
 package pairmatching.domain;
 
 public class Mission {
+    private Members members;
+    private String name;
+    private int matchCount;
+
+    public Mission(String name) {
+        this.members = new Members();
+        this.name = name;
+        this.matchCount = 0;
+    }
 }
