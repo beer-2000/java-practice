45f0d3191e54afbff819d9766e022e170350d7ca
Woojin Lee <dhkep03@gmail.com>
2022-12-16 21:38:23 +0900

    feat(BridgeGame): 재시작

--- wooteco/java-bridge-2/src/main/java/bridge/domain/BridgeGame.java
+++ wooteco/java-bridge-2/src/main/java/bridge/domain/BridgeGame.java
@@ -6,6 +6,8 @@ import bridge.BridgeRandomNumberGenerator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static bridge.domain.GameCommand.QUIT;
+import static bridge.domain.GameStatus.END_FAIL;
 import static bridge.domain.GameStatus.END_SUCCESS;
 
 /**
@@ -15,7 +17,7 @@ public class BridgeGame {
     private int tryCount;
     private GameStatus gameStatus;
     private final BridgeReferee bridgeReferee;
-    private final BridgePicture bridgePicture;
+    private BridgePicture bridgePicture;
 
     public BridgeGame(int bridgeSize) {
         this.tryCount = 1;
@@ -49,7 +51,17 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public void retry(GameCommand gameCommand) {
+        if (gameCommand.equals(QUIT)) {
+            gameStatus = END_FAIL;
+            return;
+        }
+        resetGame();
+    }
+
+    private void resetGame() {
+        this.bridgePicture = new BridgePicture();
+        tryCount += 1;
     }
 
     public boolean isOnWay() {
