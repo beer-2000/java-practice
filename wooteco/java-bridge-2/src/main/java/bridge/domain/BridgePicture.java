0a4e69010c00bbd075591d61e601b7ab9653ed6b
Woojin Lee <dhkep03@gmail.com>
2022-12-16 20:40:53 +0900

    feat(BridgePicture): 생성

--- wooteco/java-bridge-2/src/main/java/bridge/domain/BridgePicture.java
+++ wooteco/java-bridge-2/src/main/java/bridge/domain/BridgePicture.java
@@ -1,4 +1,14 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgePicture {
+    private List<Moving> bridge;
+    private boolean isFail;
+
+    public BridgePicture() {
+        this.bridge = new ArrayList<>();
+        this.isFail = true;
+    }
 }
