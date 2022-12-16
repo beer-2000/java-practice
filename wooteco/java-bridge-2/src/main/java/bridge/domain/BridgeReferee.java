0f89d998a81171bd68870d49df0585f257ddd707
Woojin Lee <dhkep03@gmail.com>
2022-12-16 20:02:22 +0900

    feat(BridgeReferee): 생성

--- wooteco/java-bridge-2/src/main/java/bridge/domain/BridgeReferee.java
+++ wooteco/java-bridge-2/src/main/java/bridge/domain/BridgeReferee.java
@@ -1,4 +1,11 @@
 package bridge.domain;
 
+import java.util.List;
+
 public class BridgeReferee {
+    private List<Moving> bridge;
+
+    public BridgeReferee(List<Moving> bridge) {
+        this.bridge = bridge;
+    }
 }
