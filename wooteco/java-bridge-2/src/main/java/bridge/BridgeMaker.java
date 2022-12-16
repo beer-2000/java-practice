8d7b1200c0c7c01e96b79040e79c2b5b65348821
Woojin Lee <dhkep03@gmail.com>
2022-12-16 21:50:31 +0900

    feat(BridgeMaker): 예외처리 -잘못된 다리 길이

--- wooteco/java-bridge-2/src/main/java/bridge/BridgeMaker.java
+++ wooteco/java-bridge-2/src/main/java/bridge/BridgeMaker.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static bridge.constant.BridgeRule.MAXIMUM_BRIDGE_SIZE;
+import static bridge.constant.BridgeRule.MINIMUM_BRIDGE_SIZE;
+
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -22,6 +25,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        if (size < MINIMUM_BRIDGE_SIZE || size > MAXIMUM_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(String.format(
+                "[ERROR] 다리 길이는 %d 이상, %d 이하여야 합니다.", MINIMUM_BRIDGE_SIZE, MAXIMUM_BRIDGE_SIZE
+            ));
+        }
         List<String> bridgeRaw = new ArrayList<>();
         IntStream.range(0, size)
             .forEach(number -> bridgeRaw.add(Moving.getCommandByNumber(bridgeNumberGenerator.generate())));
--- wooteco/java-bridge-2/src/main/java/bridge/constant/BridgeRule.java
+++ wooteco/java-bridge-2/src/main/java/bridge/constant/BridgeRule.java
@@ -1,6 +1,6 @@
 package bridge.constant;
 
 public class BridgeRule {
-    public final int MINIMUM_BRIDGE_SIZE = 3;
-    public final int MAXIMUM_BRIDGE_SIZE = 20;
+    public static final int MINIMUM_BRIDGE_SIZE = 3;
+    public static final int MAXIMUM_BRIDGE_SIZE = 20;
 }
