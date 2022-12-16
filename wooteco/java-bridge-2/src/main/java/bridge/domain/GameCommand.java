b12ede13f0317cd114faf4fa68f8f585e0521edf
Woojin Lee <dhkep03@gmail.com>
2022-12-16 21:01:35 +0900

    feat(GameCommand): 게임 재시작 커맨드 상수 집합 구현

--- wooteco/java-bridge-2/src/main/java/bridge/domain/GameCommand.java
+++ wooteco/java-bridge-2/src/main/java/bridge/domain/GameCommand.java
@@ -1,4 +1,31 @@
 package bridge.domain;
 
+import java.util.Arrays;
+
 public enum GameCommand {
+    RETRY("R"),
+    QUIT("Q");
+
+    private String command;
+
+    private static final String ERROR_MESSAGE_INCORRECT_COMMAND = "[ERROR] 잘못된 입력입니다.";
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public boolean checkCommand(String command) {
+        return this.command.equals(command);
+    }
+
+    public static GameCommand getByCommand(String command) {
+        return Arrays.stream(GameCommand.values())
+            .filter(gameCommand -> gameCommand.checkCommand(command))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_INCORRECT_COMMAND));
+    }
 }
