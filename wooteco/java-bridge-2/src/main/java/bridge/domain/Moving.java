0dadfe6dadb4c0e1f0dc8ace13ee688e46eebc79
Woojin Lee <dhkep03@gmail.com>
2022-12-16 20:01:18 +0900

    feat(Moving): 특정 커맨드의 값 반환

--- wooteco/java-bridge-2/src/main/java/bridge/domain/Moving.java
+++ wooteco/java-bridge-2/src/main/java/bridge/domain/Moving.java
@@ -1,10 +1,14 @@
 package bridge.domain;
 
+import java.util.Arrays;
+
 public enum Moving {
     UP("U"),
     DOWN("D");
 
     private String command;
+
+    private static final String ERROR_MESSAGE_INCORRECT_COMMAND = "[ERROR] 잘못된 이동 방향입니다.";
 
     Moving(String command) {
         this.command = command;
@@ -12,5 +16,12 @@ public enum Moving {
 
     public boolean checkCommand(String command) {
         return this.command.equals(command);
+    }
+
+    public static Moving getByCommand(String command) {
+        return Arrays.stream(Moving.values())
+            .filter(moving -> moving.checkCommand(command))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_INCORRECT_COMMAND));
     }
 }
