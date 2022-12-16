4e271bbe84189f63cfad430120b6e1dd67ef3fb9
Woojin Lee <dhkep03@gmail.com>
2022-12-16 20:10:38 +0900

    feat(Moving): 특정 숫자의 값 반환

--- wooteco/java-bridge-2/src/main/java/bridge/domain/Moving.java
+++ wooteco/java-bridge-2/src/main/java/bridge/domain/Moving.java
@@ -3,19 +3,26 @@ package bridge.domain;
 import java.util.Arrays;
 
 public enum Moving {
-    UP("U"),
-    DOWN("D");
+    UP("U", 1),
+    DOWN("D", 0);
 
     private String command;
+    private int inclusive;
 
     private static final String ERROR_MESSAGE_INCORRECT_COMMAND = "[ERROR] 잘못된 이동 방향입니다.";
+    private static final String ERROR_MESSAGE_INCORRECT_INCLUSIVE = "[ERROR] 이동에 대한 잘못된 숫자입니다.";
 
-    Moving(String command) {
+    Moving(String command, int inclusive) {
         this.command = command;
+        this.inclusive = inclusive;
     }
 
     public boolean checkCommand(String command) {
         return this.command.equals(command);
+    }
+
+    public boolean checkInclusive(int inclusive) {
+        return this.inclusive == inclusive;
     }
 
     public static Moving getByCommand(String command) {
@@ -23,5 +30,13 @@ public enum Moving {
             .filter(moving -> moving.checkCommand(command))
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_INCORRECT_COMMAND));
+    }
+
+    public static String getCommandByNumber(int inclusive) {
+        Moving movingTarget = Arrays.stream(Moving.values())
+            .filter(moving -> moving.checkInclusive(inclusive))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_INCORRECT_INCLUSIVE));
+        return movingTarget.command;
     }
 }
