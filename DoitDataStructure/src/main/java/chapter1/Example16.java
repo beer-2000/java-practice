a26b75c140362c44dbfd8fc89718da6e060f0d95
Woojin Lee <dhkep03@gmail.com>
2023-01-14 00:10:17 +0900

    Do it 자료구조 1장 끝

---
+++ DoitDataStructure/src/main/java/chapter1/Example16.java
@@ -0,0 +1,34 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class Example16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.print("숫자를 입력하세요 : ");
+        n = scanner.nextInt();
+        while (n <= 0) {
+            System.out.println("0보다 큰 수를 입력하세요");
+            System.out.print("숫자를 입력하세요 : ");
+            n = scanner.nextInt();
+        }
+
+        spira(n);
+    }
+
+    private static void spira(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(i % 10);
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+}
