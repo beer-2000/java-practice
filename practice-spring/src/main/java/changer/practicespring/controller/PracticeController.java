3afbcdd424768df6666d23772fc3a99b3c8a7abf
Woojin Lee <dhkep03@gmail.com>
2023-01-15 01:30:02 +0900

    Spring 기초 강의 섹션 2 완료

--- practice-spring/src/main/java/changer/practicespring/controller/PracticeController.java
+++ practice-spring/src/main/java/changer/practicespring/controller/PracticeController.java
@@ -3,6 +3,8 @@ package changer.practicespring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PracticeController {
@@ -12,5 +14,37 @@ public class PracticeController {
         model.addAttribute("data", "hello!!");
         model.addAttribute("data2", "Cool!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name", required = true) String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
