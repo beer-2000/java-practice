788777a641d7f8567b5f5b5b4625596a7c7a47d8
Woojin Lee <dhkep03@gmail.com>
2023-01-14 22:59:38 +0900

    init: 프로젝트 초기 세팅

---
+++ practice-spring/src/main/java/changer/practicespring/PracticeSpringApplication.java
@@ -0,0 +1,13 @@
+package changer.practicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringApplication.class, args);
+	}
+
+}
---
+++ practice-spring/src/test/java/changer/practicespring/PracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package changer.practicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
